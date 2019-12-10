bb8798d8c67813bceb0ce2879b106e7d62d798f6
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-11 06:34:04 +0900

    feat(app): welcomeとtosのモジュールとコンポーネントを作成 #1#8

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'welcome',
+    loadChildren:  () => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path: 'tos',
+    loadChildren:  () => import('./tos/tos.module').then(m => m.TosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
