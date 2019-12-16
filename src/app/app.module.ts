7e2fc94c849f1d5833c204a8c7f230cd5e09f815
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-17 00:11:55 +0900

    refactor(app): ヘッダーとフッターのモジュール削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header/header.component';
-import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome/welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
