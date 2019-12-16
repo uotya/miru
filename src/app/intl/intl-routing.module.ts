406dd4f4f5b3ef5726bb5c37860320259602685e
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-17 02:40:52 +0900

    refactor(legal): legalコンポーネント追加 #6

--- src/app/intl/intl-routing.module.ts
+++ src/app/intl/intl-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TosComponent } from './tos/tos.component';
+import { LegalComponent } from './legal/legal.component';
 
 const routes: Routes = [
   {
     path: 'tos',
     pathMatch: 'full',
     component: TosComponent
+  },
+  {
+    path: 'legal',
+    pathMatch: 'full',
+    component: LegalComponent
   }
 ];
 
--- src/app/intl/intl.module.ts
+++ src/app/intl/intl.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { IntlRoutingModule } from './intl-routing.module';
 import { TosComponent } from './tos/tos.component';
+import { LegalComponent } from './legal/legal.component';
 
 @NgModule({
-  declarations: [TosComponent],
+  declarations: [TosComponent, LegalComponent],
   imports: [CommonModule, IntlRoutingModule]
 })
 export class IntlModule {}
