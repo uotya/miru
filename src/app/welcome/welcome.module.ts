39c035c0b0af2f643c62584ed587ebb3b62b1ba2
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-17 12:08:33 +0900

    refactor(welcome): cardの独立に伴う変更 #1

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -5,10 +5,11 @@ import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule, SharedModule],
+  imports: [CommonModule, WelcomeRoutingModule, SharedModule, MatButtonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class WelcomeModule {}
