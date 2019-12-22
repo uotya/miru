f78412a2ea22294d7af6081f80570c9a7e43f265
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-23 06:29:03 +0900

    feat(header): リロード問題解消、スタイル調整

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { map, tap } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  user$ = this.authService.afUser$;
+  isUserChecked: boolean;
+  user$ = this.authService.afUser$.pipe(
+    tap(() => {
+      this.isUserChecked = true;
+    })
+  );
   isLoading: boolean;
 
   constructor(private authService: AuthService, private router: Router) {}
