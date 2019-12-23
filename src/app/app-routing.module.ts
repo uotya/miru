b181451b6133991a9b6adb1cea1c7fa57a1fb578
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-24 07:31:54 +0900

    feat(guards): 未認証ガードの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
+import { GuestGuard } from './guards/guest.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadChildren: () =>
-      import('./welcome/welcome.module').then(m => m.WelcomeModule)
+      import('./welcome/welcome.module').then(m => m.WelcomeModule),
+    canLoad: [GuestGuard],
+    canActivate: [GuestGuard]
   },
   {
     path: 'create',
     loadChildren: () =>
-      import('./create/create.module').then(m => m.CreateModule)
+      import('./create/create.module').then(m => m.CreateModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard]
   },
   {
     path: 'intl',
--- src/app/create/create-routing.module.ts
+++ src/app/create/create-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateComponent } from './create/create.component';
-import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    component: CreateComponent,
-    canLoad: [AuthGuard],
-    canActivate: [AuthGuard]
+    component: CreateComponent
   }
 ];
 
