e41cb8d1283b85d43f9ffa881148ef1969d9527f
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2019-12-26 03:31:03 +0900

    feat(create): 投稿画面の見た目実装 #2

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -3,9 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create/create.component';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [CreateComponent],
-  imports: [CommonModule, CreateRoutingModule]
+  imports: [
+    CommonModule,
+    CreateRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatTooltipModule
+  ]
 })
 export class CreateModule {}
