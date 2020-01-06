1ac6fc0b685e7e73c4d8b342fff244837a8a0422
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-06 16:14:44 +0900

    refactor(mylist): マイリスト欄の独立

--- src/app/create/create/create.component.ts
+++ src/app/create/create/create.component.ts
@@ -25,17 +25,6 @@ export class CreateComponent implements OnInit {
     links: this.fb.array([this.linkFormGroup])
   });
 
-  card: Card = {
-    avatarURL:
-      'https://saruwakakun.com/wp-content/uploads/2017/06/bdrArtwork.jpg',
-    userName: 'しばいぬ',
-    title:
-      '初めて犬を飼うときに役に立つリンクをまとめました初めて犬を飼うときに役に立つリンクをまとめました',
-    thumbURL:
-      'https://saruwakakun.com/wp-content/uploads/2017/06/dogg-03-min.png',
-    favorite: 100
-  };
-
   get titleControl() {
     return this.form.get('title') as FormControl;
   }
---
+++ src/app/create/mylist/mylist.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/interfaces/card';
+
+@Component({
+  selector: 'app-mylist',
+  templateUrl: './mylist.component.html',
+  styleUrls: ['./mylist.component.scss']
+})
+export class MylistComponent implements OnInit {
+  card: Card = {
+    avatarURL:
+      'https://saruwakakun.com/wp-content/uploads/2017/06/bdrArtwork.jpg',
+    userName: 'しばいぬ',
+    title:
+      '初めて犬を飼うときに役に立つリンクをまとめました初めて犬を飼うときに役に立つリンクをまとめました',
+    thumbURL:
+      'https://saruwakakun.com/wp-content/uploads/2017/06/dogg-03-min.png',
+    favorite: 100
+  };
+
+  constructor() {}
+
+  ngOnInit() {}
+}
