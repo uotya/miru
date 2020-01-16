65b72285cf37c012ecc057d0af76f63b47f83cb4
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-16 18:00:31 +0900

    refactor(welcome): スタイル調整、不要な記述の削除

--- src/app/shared/ranking/ranking.component.ts
+++ src/app/shared/ranking/ranking.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleWithUser } from 'src/app/interfaces/article-with-user';
-import { Article } from 'src/app/interfaces/article';
 import { Observable } from 'rxjs';
 import { ArticleService } from 'src/app/services/article.service';
-import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ranking',
@@ -14,24 +12,6 @@ export class RankingComponent implements OnInit {
   articles$: Observable<
     ArticleWithUser[]
   > = this.articleService.getPopularArticles();
-
-  card: ArticleWithUser = {
-    author: {
-      uid: 'temporary',
-      userName: 'しばいぬ',
-      avatarURL:
-        'https://saruwakakun.com/wp-content/uploads/2017/06/bdrArtwork.jpg'
-    },
-    authorId: 'temporary',
-    createdAt: new Date(),
-    title:
-      '初めて犬を飼うときに役に立つリンクをまとめました初めて犬を飼うときに役に立つリンクをまとめました',
-    description: 'temporary',
-    links: [],
-    thumbnailURL:
-      'https://saruwakakun.com/wp-content/uploads/2017/06/dogg-03-min.png',
-    favorite: 100
-  };
 
   constructor(private articleService: ArticleService) {}
 
