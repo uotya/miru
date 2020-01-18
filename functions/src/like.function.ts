3b0fde8b2d26d782c3dd74aa9dbaf79e098c4668
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-19 00:54:01 +0900

    feat(like): いいね機能の関数を実装 #17

---
+++ functions/src/like.function.ts
@@ -0,0 +1,37 @@
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { shouldEventRun, markEventTried } from './utility.function';
+
+const db = admin.firestore();
+
+export const countUpFavorite = functions.firestore
+  .document('users/{userId}/likeArticles/{articleId}')
+  .onCreate(async (snap, context) => {
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should: boolean) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('favorite', admin.firestore.FieldValue.increment(1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
+  });
+
+export const countDownFavorite = functions.firestore
+  .document('users/{userId}/likeArticles/{articleId}')
+  .onDelete(async (snap, context) => {
+    const eventId = context.eventId;
+    return shouldEventRun(eventId).then(async (should: boolean) => {
+      if (should) {
+        await db
+          .doc(`articles/${context.params.articleId}`)
+          .update('favorite', admin.firestore.FieldValue.increment(-1));
+        return markEventTried(eventId);
+      } else {
+        return;
+      }
+    });
+  });
