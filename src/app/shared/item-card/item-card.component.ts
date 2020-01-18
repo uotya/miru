f38968e9ac40d1ea4a4e25a97f71fded59e45421
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-19 00:57:26 +0900

    feat(item-card): いいねボタンの実装 #17

--- src/app/shared/item-card/item-card.component.ts
+++ src/app/shared/item-card/item-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ArticleWithUser } from 'src/app/interfaces/article-with-user';
 import { LikeService } from 'src/app/services/like.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-item-card',
@@ -11,7 +12,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class ItemCardComponent implements OnInit {
   @Input() card: ArticleWithUser;
   isLiked: boolean;
-  uid = this.authService.user.uid;
+  favorite: number;
 
   constructor(
     private likeService: LikeService,
@@ -19,25 +20,30 @@ export class ItemCardComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.likeService
-      .isLiked(this.card.articleId, this.uid)
-      .subscribe(result => {
-        this.isLiked = result;
-      });
+    this.favorite = this.card.favorite;
+    if (this.authService.user) {
+      this.likeService
+        .isLiked(this.card.articleId, this.authService.user.uid)
+        .pipe(take(1))
+        .subscribe(result => {
+          this.isLiked = result;
+        });
+    }
   }
 
   clickedLike(article: ArticleWithUser) {
     const articleId = article.articleId;
-    if (!this.isLiked) {
-      this.likeService.likeArticle(articleId, this.uid);
+    const user = this.authService.user;
+    if (user && !this.isLiked) {
+      this.likeService.likeArticle(articleId, user.uid);
+      this.favorite++;
+      this.isLiked = true;
+    } else if (user && this.isLiked) {
+      this.likeService.deleteLikeArticle(articleId, user.uid);
+      this.favorite--;
+      this.isLiked = false;
     } else {
-      this.likeService.deleteLikeArticle(articleId, this.uid);
+      alert('ログインするといいねが押せます');
     }
-  }
-
-  checkLiked(articleId: string) {
-    this.likeService.isLiked(articleId, this.uid).subscribe(result => {
-      this.isLiked = result;
-    });
   }
 }
