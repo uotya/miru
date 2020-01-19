04fc5a0d409dbc8b99d27a52de32f144f5fa7e98
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-19 16:22:34 +0900

    refactor(index.ts): 不要なコメントの削除

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,3 @@
 export { createUser } from './user.function';
 export { countUpFavorite } from './like.function';
 export { countDownFavorite } from './like.function';
-
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
