541cc7b3880f32adebd98dfc015d99a38f48aa3a
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-01-20 23:22:10 +0900

    feat(article): パスを設定

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticleComponent } from './article/article.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ArticleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
