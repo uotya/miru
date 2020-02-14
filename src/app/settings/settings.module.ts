83e62c47e280556fc6653f282615ec29275189fe
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-02-14 11:50:00 +0900

    feat(settings): 中身の実装 #13

--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,9 +4,21 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings/settings.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { SharedModule } from '../shared/shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { DeleteAccountDialogComponent } from './delete-account-dialog/delete-account-dialog.component';
 
 @NgModule({
-  declarations: [SettingsComponent],
-  imports: [CommonModule, SettingsRoutingModule, MatTabsModule]
+  declarations: [SettingsComponent, DeleteAccountDialogComponent],
+  imports: [
+    CommonModule,
+    SettingsRoutingModule,
+    MatTabsModule,
+    SharedModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
 })
 export class SettingsModule {}
--- src/app/settings/settings/settings.component.ts
+++ src/app/settings/settings/settings.component.ts
@@ -1,5 +1,10 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from 'src/app/services/loading.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Validators, FormControl } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DeleteAccountDialogComponent } from '../delete-account-dialog/delete-account-dialog.component';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +12,36 @@ import { LoadingService } from 'src/app/services/loading.service';
   styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
-  constructor(private loadingService: LoadingService) {}
+  userId = this.authService.user.uid;
+  user$ = this.authService.getUserData(this.userId);
+
+  nameForm = new FormControl('', [
+    Validators.required,
+    Validators.maxLength(30)
+  ]);
+
+  constructor(
+    private loadingService: LoadingService,
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.loadingService.toggleLoading(false);
+  }
+
+  changeUserName() {
+    const newName: string = this.nameForm.value;
+    this.authService.changeUserName(this.userId, newName);
+    this.snackBar.open('変更されました！', null, {
+      duration: 2000
+    });
+  }
+
+  deleteAccount() {
+    this.dialog.open(DeleteAccountDialogComponent, {
+      restoreFocus: false
+    });
   }
 }
