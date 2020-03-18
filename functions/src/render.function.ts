ed6c60a1c0d3223d4d6ebea4946bc2dda40deebb
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-03-18 21:07:05 +0900

    feat(render.function): ダイナミックレンダリングの実装

---
+++ functions/src/render.function.ts
@@ -0,0 +1,43 @@
+const functions = require('firebase-functions');
+const express = require('express');
+const fetch = require('node-fetch');
+const url = require('url');
+const useragent = require('express-useragent');
+
+const appUrl =
+  functions.confing().env.mode === 'prod'
+    ? 'miru-prod.web.app'
+    : 'miru-2ac6c.web.app';
+
+const generateUrl = (req: any) => {
+  return url.format({
+    protocol: 'https',
+    host: appUrl,
+    pathname: req.originalUrl
+  });
+};
+
+const app = express();
+
+app.use(useragent.express());
+
+app.get('*', async (req: any, res: any) => {
+  if (req.useragent.isBot) {
+    const rendertronAppURL = 'https://rendertron-uotya.appspot.com/';
+    const response = await fetch(
+      `${rendertronAppURL}/render/${generateUrl(req)}`
+    );
+    const body = await response.text();
+    res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
+    res.set('Vary', 'User-Agent');
+    res.send(body.toString());
+  } else {
+    fetch(`https://${appUrl}`)
+      .then((result: any) => result.text())
+      .then((body: any) => {
+        res.send(body.toString());
+      });
+  }
+});
+
+export const render = functions.https.onRequest(app);
