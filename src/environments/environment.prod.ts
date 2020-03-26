6f40fc68fa7301d54735915dd0d0d5a90c93823b
Haruka Kawaguchi <kawaguchi.ha@gmail.com>
2020-03-26 17:01:22 +0900

    feat(firebase): 本番用のプロジェクトを追加

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,17 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyCb-FMkKd5Q_E23K_M3fKQ9AfJ7PxD472I',
-    authDomain: 'miru-2ac6c.firebaseapp.com',
-    databaseURL: 'https://miru-2ac6c.firebaseio.com',
-    projectId: 'miru-2ac6c',
-    storageBucket: 'miru-2ac6c.appspot.com',
-    messagingSenderId: '1085352716089',
-    appId: '1:1085352716089:web:ac12f2f621841e6a69aae8',
-    measurementId: 'G-PCF1R7STR2'
+    apiKey: 'AIzaSyAWSBAmHvICs_zLM2hVnK8nGtyVWESa518',
+    authDomain: 'miru-prod.firebaseapp.com',
+    databaseURL: 'https://miru-prod.firebaseio.com',
+    projectId: 'miru-prod',
+    storageBucket: 'miru-prod.appspot.com',
+    messagingSenderId: '344711606755',
+    appId: '1:344711606755:web:5aa463ecd652e3f66830d6',
+    measurementId: 'G-Q93W4X0TSJ'
+  },
+  algolia: {
+    appId: 'D1EI3EJXJE',
+    apiKey: 'cc7a4b206f69429787601043ef18ca08'
   }
 };
